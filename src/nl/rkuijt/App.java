8d741c7f914dea8e6f3d24b8b93be68c4595fd41
wierish <43202580+wierish@users.noreply.github.com>
2020-02-12 23:27:28 +0100

    Update App.java
    int size adjustment

--- src/nl/rkuijt/App.java
+++ src/nl/rkuijt/App.java
@@ -21,7 +21,7 @@ public class App {
 		Goose goose = objectMapper.readValue(dataFile, Goose.class);
 
 		// Generate random size
-		int size = r.ints(0, 100).findFirst().getAsInt();
+		int size = 42;
 		goose.setSize(size);
 
 		// Write goose to file
